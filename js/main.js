f10cf420c206918b1899d6bf671be1a7e17baa64
Chandru <65664480+B-chandru@users.noreply.github.com>
2020-12-27 12:34:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var btn=document.getElementById("button");
-var div=document.getElementById("food_details");
-var div_left=document.getElementById("food_img")
-var rname=document.getElementById("rname")
-var video=document.getElementById("t_video")
+var btn=document.getElementById("btn");
+var title=document.getElementById("title");
+var div=document.getElementById("grid");
+var video=document.getElementById("bottom")
+
 
 btn.addEventListener("click",()=>{
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
@@ -11,27 +11,21 @@ btn.addEventListener("click",()=>{
 
 })
 
-
-function displayMeals(meals) {
-   rname.innerHTML=`${meals.strMeal}`
-    div.innerHTML=`<div class="contain">
-    <p id="p"> <strong>INSTRUCTION : </strong>${meals.strInstructions}<p>
-    </div>`
-   div_left.innerHTML=`<div class="info">
-   <img src="${meals.strMealThumb}"style="border-radius:50%;margin-top: 30px;"alt="meals pic" width="350px" height="300px"/>
-   <p style="text-align:center;
-   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"> <strong style="text-align:center;
-   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">CATEGORY : </strong>${meals.strCategory}</p>
-   <p style="text-align:center;
-   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"> <strong style="text-align:center;
-   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">AREA : </strong>${meals.strArea}<p>
-    <p style="text-align:center;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"> <strong style="text-align:center;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">TAGS  : </strong>${meals.strTags}<p>
-
-    </div>`
-    video.innerHTML=`
-    <iframe src="https://www.youtube.com/embed/${meals.strYoutube.slice(-11)}" width="600px" height="400px" ></iframe> `
-  
-   
+function displayMeals(meals){
+ title.innerHTML=`${meals.strMeal}`
+ div.innerHTML=`  <div class="g1">
+ <img src="${meals.strMealThumb}" alt="" id="img">
+ <p id="cat"><strong style=" text-shadow: 0px -1px 0px white;">CATAGORY:
+ </strong>${meals.strCategory}</p>
+ <p id="AREA"><strong style="text-shadow: 0px -1px 0px white;">AREA:
+ </strong>${meals.strArea}</p>
+ <p id="cat"><strong style="text-shadow: 0px -1px white;">TAGS:</strong style="text-shadow: 0px 1px 0px white;">${meals.strTags}<p>
+    </strong></p>
+</div>
+<div class="g2">
+ <h2 id="in">instruction</h2>
+    <p> ${meals.strInstructions}<p>
+</div>`
+video.innerHTML=`<h2 id="h4">VIDEO TUTORIAL OF THIS RECEPIE</h2>
+<iframe src="https://www.youtube.com/embed/${meals.strYoutube.slice(-11)}" width="500px" height="350px" id="if"></iframe> `;
 }
